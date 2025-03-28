c3c5a77963560f060b8d3159d81efebe166f5b84
swizra <195688945+swizra@users.noreply.github.com>
2025-03-28 12:29:08 +0100

    Update dynamic-dates.js

--- js/dynamic-dates.js
+++ js/dynamic-dates.js
@@ -5,16 +5,20 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     const today = new Date();
+
     const event1Date = new Date(today);
-    event1Date.setDate(today.getDate() + 14); 
+    event1Date.setDate(today.getDate() + 14);
     document.getElementById("event-date-1").querySelector("span").textContent = formatDate(event1Date);
+
     const event2Date = new Date(today);
-    event2Date.setDate(today.getDate() + 8); 
+    event2Date.setDate(today.getDate() + 8);
     document.getElementById("event-date-2").querySelector("span").textContent = formatDate(event2Date);
+
     const event3Date = new Date(today);
-    event3Date.setDate(today.getDate() + 7); 
+    event3Date.setDate(today.getDate() + 7);
     document.getElementById("event-date-3").querySelector("span").textContent = formatDate(event3Date);
+
     const event4Date = new Date(today);
-    event4Date.setDate(today.getDate() + 15); 
+    event4Date.setDate(today.getDate() + 15);
     document.getElementById("event-date-4").querySelector("span").textContent = formatDate(event4Date);
 });
